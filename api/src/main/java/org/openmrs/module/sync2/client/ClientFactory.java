bd70b0bb56393f8da6317d9f1c72cc4e5d6e01e3
Jakub Trzebiatowski <jtrzebiatowski@soldevelo.com>
2017-11-29 15:37:25 +0100

    FM-240: Code improvements

--- api/src/main/java/org/openmrs/module/sync2/client/ClientFactory.java
+++ api/src/main/java/org/openmrs/module/sync2/client/ClientFactory.java
@@ -1,13 +1,14 @@
 package org.openmrs.module.sync2.client;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.openmrs.module.fhir.api.client.Client;
-import org.openmrs.module.fhir.api.client.fhir.FHIRClientFactory;
+import org.openmrs.module.fhir.api.client.FHIRClientFactory;
+import org.openmrs.module.sync2.client.reader.SyncFeedWorker;
 import org.openmrs.module.sync2.client.rest.RestClientFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ClientFactory {
-    protected final Log log = LogFactory.getLog(this.getClass());
+    private static final Logger LOGGER = LoggerFactory.getLogger(SyncFeedWorker.class);
 
     private static final String REST_CLIENT_KEY = "rest";
     private static final String FHIR_CLIENT_KEY = "fhir";
@@ -20,7 +21,7 @@ public class ClientFactory {
             case FHIR_CLIENT_KEY:
                 return FHIRClientFactory.createClient();
             default:
-                log.warn(String.format("Unrecognized clientType: %s", clientType));
+                LOGGER.warn(String.format("Unrecognized clientType: %s", clientType));
                 return null;
         }
     }
