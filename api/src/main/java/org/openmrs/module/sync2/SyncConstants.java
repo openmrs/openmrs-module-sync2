cccb6e7e86fc19a77beb39e5dc4e73b5d48a4936
Arkadiusz Lalo <alalo@soldevelo.com>
2017-12-08 20:57:13 +0100

    SYNCT-55: Added auditing to the pull service

--- api/src/main/java/org/openmrs/module/sync2/SyncConstants.java
+++ api/src/main/java/org/openmrs/module/sync2/SyncConstants.java
@@ -6,4 +6,6 @@ public class SyncConstants {
     public static final String SYNC2_PATH_TO_CUSTOM_CONFIGURATION = "customSyncConfiguration.json";
 
     public static final String ID_COLUMN_NAME = "id";
+
+    public static final String PULL_SUCCESS_MESSAGE = "Success";
 }
--- api/src/main/java/org/openmrs/module/sync2/api/impl/SyncPullServiceImpl.java
+++ api/src/main/java/org/openmrs/module/sync2/api/impl/SyncPullServiceImpl.java
@@ -1,22 +1,33 @@
 package org.openmrs.module.sync2.api.impl;
 
+import org.openmrs.module.sync2.api.SyncAuditService;
 import org.openmrs.module.sync2.api.SyncPullService;
 import org.openmrs.module.sync2.api.sync.SyncClient;
 import org.openmrs.module.sync2.api.sync.SyncPersistence;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
+import static org.openmrs.module.sync2.SyncConstants.PULL_SUCCESS_MESSAGE;
+
 @Component("sync2.syncPullService")
 public class SyncPullServiceImpl implements SyncPullService {
+
+    @Autowired
+    SyncAuditService auditService;
 
     private SyncClient syncClient = new SyncClient();
     private SyncPersistence syncPersistence = new SyncPersistence();
 
     public void pullDataFromParentAndSave(String category, Map<String, String> resourceLinks, String address, String action) {
 
-        Object pulledObject = syncClient.pullDataFromParent(category, resourceLinks, address);
-
-        syncPersistence.persistRetrievedData(pulledObject, action);
+        try {
+            Object pulledObject = syncClient.pullDataFromParent(category, resourceLinks, address);
+            syncPersistence.persistRetrievedData(pulledObject, action);
+            auditService.saveSuccessfulAudit(category, address, action, PULL_SUCCESS_MESSAGE);
+        } catch (Exception e) {
+            auditService.saveFailedAudit(category, address, action, e.getMessage());
+        }
     }
 }
