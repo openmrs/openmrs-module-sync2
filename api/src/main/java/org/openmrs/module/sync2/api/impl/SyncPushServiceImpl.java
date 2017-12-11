2bf6be1909fc4078585e1446058565be5bffb109
Jakub Trzebiatowski <jtrzebiatowski@soldevelo.com>
2017-12-11 20:48:48 +0100

    SYNCT-56: Implement initial push sync via fhir

--- api/src/main/java/org/openmrs/module/sync2/api/impl/SyncPushServiceImpl.java
+++ api/src/main/java/org/openmrs/module/sync2/api/impl/SyncPushServiceImpl.java
@@ -1,11 +1,14 @@
 package org.openmrs.module.sync2.api.impl;
 
+import org.openmrs.api.context.Context;
+import org.openmrs.module.sync2.api.SyncConfigurationService;
 import org.openmrs.module.sync2.api.SyncPushService;
 import org.openmrs.module.sync2.api.sync.SyncClient;
 import org.openmrs.module.sync2.api.sync.SyncPersistence;
 import org.openmrs.module.sync2.api.utils.SyncUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
@@ -16,6 +19,9 @@ public class SyncPushServiceImpl implements SyncPushService {
 
     private static final String RESOURCE_PREFERRED_CLIENT = "sync2.resource.preferred.client";
 
+    @Autowired
+    private SyncConfigurationService configurationService;
+
     private SyncClient syncClient = new SyncClient();
     private SyncPersistence syncPersistence = new SyncPersistence();
 
@@ -24,11 +30,15 @@ public class SyncPushServiceImpl implements SyncPushService {
         LOGGER.info(String.format("SyncPushService category: %s, address: %s, action: %s", category, address, action));
         // TODO: stub!
 
-        String preferredClient = "fhir";
+        String preferredClient = Context.getAdministrationService().getGlobalProperty(RESOURCE_PREFERRED_CLIENT);
 
         String uuid = SyncUtils.extractUUIDFromResourceLinks(resourceLinks);
         Object data = syncPersistence.retrieveData(preferredClient, category, uuid);
 
-        syncClient.pushDataToParent(data, resourceLinks, address);
+        syncClient.pushDataToParent(data, resourceLinks, getParentUri());
+    }
+
+    private String getParentUri() {
+        return configurationService.getSyncConfiguration().getGeneral().getParentFeedLocation();
     }
 }
