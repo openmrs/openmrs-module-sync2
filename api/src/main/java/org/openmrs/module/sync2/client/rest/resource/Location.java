c5492d0359328c7791aa3163330368f96c75ee15
Jakub Trzebiatowski <jtrzebiatowski@soldevelo.com>
2017-12-14 16:56:35 +0100

    SYNCT-60: Distinguish ref location and full location. Add missing fields

--- api/src/main/java/org/openmrs/module/sync2/client/rest/resource/Location.java
+++ api/src/main/java/org/openmrs/module/sync2/client/rest/resource/Location.java
@@ -8,7 +8,30 @@ import java.util.List;
 public class Location implements RestResource {
     private String uuid;
     private String display;
+    private String name;
+    private String description;
+    private String cityVillage;
+    private String stateProvince;
+    private String country;
+    private String postalCode;
+    private String latitude;
+    private String longitude;
+    private String countryDistrict;
+    private String address1;
+    private String address2;
+    private String address3;
+    private String address4;
+    private String address5;
+    private String address6;
+    private Location parentLocation;
+    private List<Location> childLocations;
+    private Boolean retired;
+
+    // List<LocationAttribute> attributes
+
     private List<Link> links;
+
+    private String resourceVersion;
 
     // region getters
     public String getUuid() {
@@ -19,9 +42,86 @@ public class Location implements RestResource {
         return display;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getCityVillage() {
+        return cityVillage;
+    }
+
+    public String getStateProvince() {
+        return stateProvince;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public String getLatitude() {
+        return latitude;
+    }
+
+    public String getLongitude() {
+        return longitude;
+    }
+
+    public String getCountryDistrict() {
+        return countryDistrict;
+    }
+
+    public String getAddress1() {
+        return address1;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public String getAddress3() {
+        return address3;
+    }
+
+    public String getAddress4() {
+        return address4;
+    }
+
+    public String getAddress5() {
+        return address5;
+    }
+
+    public String getAddress6() {
+        return address6;
+    }
+
+    public Location getParentLocation() {
+        return parentLocation;
+    }
+
+    public List<Location> getChildLocations() {
+        return childLocations;
+    }
+
+    public Boolean getRetired() {
+        return retired;
+    }
+
     public List<Link> getLinks() {
         return links;
     }
+
+    public String getResourceVersion() {
+        return resourceVersion;
+    }
+
     // endregion
 
     // region setters
@@ -33,14 +133,123 @@ public class Location implements RestResource {
         this.display = display;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setCityVillage(String cityVillage) {
+        this.cityVillage = cityVillage;
+    }
+
+    public void setStateProvince(String stateProvince) {
+        this.stateProvince = stateProvince;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public void setLatitude(String latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setLongitude(String longitude) {
+        this.longitude = longitude;
+    }
+
+    public void setCountryDistrict(String countryDistrict) {
+        this.countryDistrict = countryDistrict;
+    }
+
+    public void setAddress1(String address1) {
+        this.address1 = address1;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+
+    public void setAddress3(String address3) {
+        this.address3 = address3;
+    }
+
+    public void setAddress4(String address4) {
+        this.address4 = address4;
+    }
+
+    public void setAddress5(String address5) {
+        this.address5 = address5;
+    }
+
+    public void setAddress6(String address6) {
+        this.address6 = address6;
+    }
+
+    public void setParentLocation(Location parentLocation) {
+        this.parentLocation = parentLocation;
+    }
+
+    public void setChildLocations(List<Location> childLocations) {
+        this.childLocations = childLocations;
+    }
+
+    public void setRetired(Boolean retired) {
+        this.retired = retired;
+    }
+
     public void setLinks(List<Link> links) {
         this.links = links;
     }
+
+    public void setResourceVersion(String resourceVersion) {
+        this.resourceVersion = resourceVersion;
+    }
+
     // endregion
 
     @Override
     public BaseOpenmrsObject getOpenMrsObject() {
-        org.openmrs.Location omrsLocation = Context.getLocationService().getLocationByUuid(uuid);
+        if (name == null) {
+            // This instance of location represents GET ref location, so we have to fetch it from the persistence.
+            return Context.getLocationService().getLocationByUuid(uuid);
+        }
+        org.openmrs.Location omrsLocation = new org.openmrs.Location();
+        omrsLocation.setUuid(uuid);
+        omrsLocation.setName(name);
+        omrsLocation.setDescription(description);
+        omrsLocation.setCityVillage(cityVillage);
+        omrsLocation.setStateProvince(stateProvince);
+        omrsLocation.setCountry(country);
+        omrsLocation.setPostalCode(postalCode);
+        omrsLocation.setLatitude(latitude);
+        omrsLocation.setLongitude(longitude);
+        omrsLocation.setCountyDistrict(countryDistrict);
+        omrsLocation.setAddress1(address1);
+        omrsLocation.setAddress2(address2);
+        omrsLocation.setAddress3(address3);
+        omrsLocation.setAddress4(address4);
+        omrsLocation.setAddress5(address5);
+        omrsLocation.setAddress6(address6);
+        if (parentLocation != null) {
+            omrsLocation.setParentLocation((org.openmrs.Location) parentLocation.getOpenMrsObject());
+        }
+        if (childLocations != null) {
+            for (Location childLocation : childLocations) {
+                omrsLocation.addChildLocation((org.openmrs.Location) childLocation.getOpenMrsObject());
+            }
+        }
+        omrsLocation.setRetired(retired);
+
+
+
         return omrsLocation;
     }
 
