73e23c35c45fb6012b6d9d392c621ab2d7afe26f
Arkadiusz Lalo <alalo@soldevelo.com>
2017-12-18 08:46:17 +0100

    SYNCT-30: Added simple operations retry logic

---
+++ api/src/main/java/org/openmrs/module/sync2/api/impl/SyncRetryServiceImpl.java
@@ -0,0 +1,56 @@
+package org.openmrs.module.sync2.api.impl;
+
+import org.openmrs.api.APIException;
+import org.openmrs.module.sync2.api.SyncConfigurationService;
+import org.openmrs.module.sync2.api.SyncPullService;
+import org.openmrs.module.sync2.api.SyncPushService;
+import org.openmrs.module.sync2.api.SyncRetryService;
+import org.openmrs.module.sync2.api.model.audit.AuditMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.openmrs.module.sync2.SyncConstants.PULL_ACTION;
+import static org.openmrs.module.sync2.SyncConstants.PUSH_ACTION;
+
+@Component("sync2.SyncRetryService")
+public class SyncRetryServiceImpl implements SyncRetryService {
+
+    @Autowired
+    private SyncPullService syncPullService;
+
+    @Autowired
+    private SyncPushService syncPushService;
+
+    @Autowired
+    private SyncConfigurationService configuration;
+
+    @Override
+    public AuditMessage retryMessage(AuditMessage message) throws APIException {
+        switch(message.getAction()) {
+            case PULL_ACTION:
+                return retryPull(message);
+            case PUSH_ACTION:
+                return retryPush(message);
+        }
+        return null;
+    }
+
+    private AuditMessage retryPush(AuditMessage message) {
+        String parentAddress = configuration.getSyncConfiguration().getGeneral().getParentFeedLocation();
+        Map<String, String> map = new HashMap<>();
+        map.put(message.getLinkType(), message.getResourceUrl());
+        message = syncPushService.readDataAndPushToParent(message.getResourceName(), map, parentAddress, message.getAction());
+        return message;
+    }
+
+    private AuditMessage retryPull(AuditMessage message) {
+        String parentAddress = configuration.getSyncConfiguration().getGeneral().getParentFeedLocation();
+        Map<String, String> map = new HashMap<>();
+        map.put(message.getLinkType(), message.getResourceUrl());
+        message = syncPullService.pullDataFromParentAndSave(message.getResourceName(), map, parentAddress, message.getAction());
+        return message;
+    }
+}
