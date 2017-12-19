6bcd7c21b94dbcb9de890b334bbcffde0f414bc4
Arkadiusz Lalo <alalo@soldevelo.com>
2017-12-19 15:57:52 +0100

    SYNCT-65: Moved client key constants to the correct file

--- api/src/main/java/org/openmrs/module/sync2/SyncConstants.java
+++ api/src/main/java/org/openmrs/module/sync2/SyncConstants.java
@@ -23,4 +23,8 @@ public class SyncConstants {
 
     public static final String PUSH_OPERATION = "PUSH";
 
+    public static final String FHIR_CLIENT_KEY = "fhir";
+
+    public static final String REST_CLIENT_KEY = "rest";
+
 }
