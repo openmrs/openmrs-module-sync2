0537737edda4630dd2cb86a1da867baa72858b5c
wojtekbu <wojtekbu@users.noreply.github.com>
2018-10-10 16:46:22 +0200

    SYNCT-185: Added a whitelist to the SYNC2 configuration (#64)
    * SYNCT-185: Added a whitelist configuration
    
    * SYNCT-185: Added unit tests for the whitelist
    configuration
    
    * SYNCT-185: Added default whitelist configuration
    
    * SYNCT-185: Moved whitelist's logic to its constructor
    
    * SYNCT-185: Renamed unit test

--- api/src/main/java/org/openmrs/module/sync2/api/model/configuration/SyncConfiguration.java
+++ api/src/main/java/org/openmrs/module/sync2/api/model/configuration/SyncConfiguration.java
@@ -7,13 +7,18 @@ public class SyncConfiguration {
     private GeneralConfiguration general;
     private SyncMethodConfiguration push;
     private SyncMethodConfiguration pull;
+    private WhitelistConfiguration whitelist;
 
-    public SyncConfiguration() { }
+    public SyncConfiguration() {
+        whitelist = new WhitelistConfiguration();
+    }
 
-    public SyncConfiguration(GeneralConfiguration general, SyncMethodConfiguration push, SyncMethodConfiguration pull) {
+    public SyncConfiguration(GeneralConfiguration general, SyncMethodConfiguration push, SyncMethodConfiguration pull,
+                             WhitelistConfiguration whitelist) {
         this.general = general;
         this.push = push;
         this.pull = pull;
+        this.whitelist = whitelist;
     }
 
     public GeneralConfiguration getGeneral() {
@@ -40,6 +45,14 @@ public class SyncConfiguration {
         this.pull = pull;
     }
 
+    public WhitelistConfiguration getWhitelist() {
+        return whitelist;
+    }
+
+    public void setWhitelist(WhitelistConfiguration whitelist) {
+        this.whitelist = whitelist;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -51,11 +64,12 @@ public class SyncConfiguration {
         SyncConfiguration that = (SyncConfiguration) o;
         return Objects.equals(general, that.general)
                 && Objects.equals(push, that.push)
-                && Objects.equals(pull, that.pull);
+                && Objects.equals(pull, that.pull)
+                && Objects.equals(whitelist, that.whitelist);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(general, push, pull);
+        return Objects.hash(general, push, pull, whitelist);
     }
 }
