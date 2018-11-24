504fd4bf0ac2c7c4ebc98043b822fcfefba29f63
Wyclif Luyima <wyclif@openmrs.org>
2018-11-24 13:58:04 -0500

    SYNCT-272 - Preserve uuids when syncing new objects (#108)

--- api/src/main/java/org/openmrs/module/sync2/client/rest/impl/RestResourceConverterImpl.java
+++ api/src/main/java/org/openmrs/module/sync2/client/rest/impl/RestResourceConverterImpl.java
@@ -10,15 +10,10 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
-import static org.openmrs.module.sync2.SyncCategoryConstants.CATEGORY_COHORT;
-import static org.openmrs.module.sync2.SyncCategoryConstants.CATEGORY_DRUG_ORDER;
 import static org.openmrs.module.sync2.SyncCategoryConstants.CATEGORY_FORM;
-import static org.openmrs.module.sync2.SyncCategoryConstants.CATEGORY_LOCATION;
 import static org.openmrs.module.sync2.SyncCategoryConstants.CATEGORY_OBSERVATION;
 import static org.openmrs.module.sync2.SyncCategoryConstants.CATEGORY_PATIENT;
 import static org.openmrs.module.sync2.SyncCategoryConstants.CATEGORY_PERSON;
-import static org.openmrs.module.sync2.SyncCategoryConstants.CATEGORY_PRIVILEGE;
-import static org.openmrs.module.sync2.SyncCategoryConstants.CATEGORY_TEST_ORDER;
 import static org.openmrs.module.sync2.SyncCategoryConstants.CATEGORY_VISIT;
 
 @Component("sync2.RestResourceConverter")
@@ -37,12 +32,6 @@ public class RestResourceConverterImpl implements RestResourceConverter {
 				case CATEGORY_PATIENT:
 					convertPatient(simpleObject);
 					break;
-				case CATEGORY_PRIVILEGE:
-					convertPrivilege(simpleObject);
-					break;
-				case CATEGORY_LOCATION:
-					convertLocation(simpleObject);
-					break;
 				case CATEGORY_OBSERVATION:
 					convertObservation(simpleObject);
 					break;
@@ -52,17 +41,8 @@ public class RestResourceConverterImpl implements RestResourceConverter {
 				case CATEGORY_FORM:
 					convertForm(simpleObject);
 					break;
-				case CATEGORY_COHORT:
-					convertCohort(simpleObject);
-					break;
 				case CATEGORY_PERSON:
 					convertPersonResource(simpleObject);
-					break;
-				case CATEGORY_DRUG_ORDER:
-					convertDrugOrder(simpleObject);
-					break;
-				case CATEGORY_TEST_ORDER:
-					convertTestOrder(simpleObject);
 					break;
 			}
 		}
@@ -148,14 +128,6 @@ public class RestResourceConverterImpl implements RestResourceConverter {
 		simpleObject.remove("preferredAddress");
 	}
 
-	private void convertPrivilege(Map<String, Object> simpleObject) {
-		simpleObject.remove("uuid");
-	}
-
-	private void convertLocation(Map<String, Object> simpleObject) {
-		simpleObject.remove("uuid");
-	}
-
 	private void convertObservation(Map<String, Object> simpleObject) {
 		Map concept = (Map<String, Object>) simpleObject.get("concept");
 		simpleObject.remove("concept");
@@ -169,17 +141,5 @@ public class RestResourceConverterImpl implements RestResourceConverter {
 	private void convertForm(SimpleObject simpleObject) {
 		simpleObject.remove("formFields");
 		simpleObject.remove("resources");
-	}
-
-	private void convertCohort(Map<String, Object> simpleObject) {
-		simpleObject.remove("uuid");
-	}
-
-	private void convertDrugOrder(Map<String, Object> simpleObject) {
-		simpleObject.remove("uuid");
-	}
-
-	private void convertTestOrder(Map<String, Object> simpleObject) {
-		simpleObject.remove("uuid");
 	}
 }
