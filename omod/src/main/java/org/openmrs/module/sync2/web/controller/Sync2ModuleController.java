cbbc67ef82bfd126d077547542691715b3fbb4e1
rstencel <rstencel@soldevelo.com>
2018-12-10 15:26:07 +0100

    Added 4 action buttons, added controllers for manual push and pull

--- omod/src/main/java/org/openmrs/module/sync2/web/controller/Sync2ModuleController.java
+++ omod/src/main/java/org/openmrs/module/sync2/web/controller/Sync2ModuleController.java
@@ -10,6 +10,10 @@
 package org.openmrs.module.sync2.web.controller;
 
 import org.openmrs.api.context.Context;
+import org.openmrs.module.sync2.api.exceptions.SyncValidationException;
+import org.openmrs.module.sync2.api.utils.ContextUtils;
+import org.openmrs.module.sync2.client.reader.LocalFeedReader;
+import org.openmrs.module.sync2.client.reader.ParentFeedReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -25,5 +29,33 @@ public class Sync2ModuleController {
 	@RequestMapping(value = "/module/sync2/sync2", method = RequestMethod.GET)
 	public void manage(ModelMap model) {
 		model.addAttribute("user", Context.getAuthenticatedUser());
+	}
+
+	@RequestMapping(value = "/module/sync2/manualPush")
+	public String manualPush() {
+		try {
+			LOGGER.info("Start Local Feed Reader...");
+			LocalFeedReader localFeedReader = ContextUtils.getLocalFeedReader();
+			localFeedReader.readAndPushAllFeeds();
+		} catch (SyncValidationException e) {
+			LOGGER.error("Error during pushing objects: ", e);
+		} catch (Exception e) {
+			LOGGER.error("Error during pushing objects: ", e);
+		}
+		return "module/admin";
+	}
+
+	@RequestMapping(value = "/module/sync2/manualPull")
+	public String manualPull() {
+		try {
+			LOGGER.info("Start Parent Feed Reader...");
+			ParentFeedReader parentFeedReader = ContextUtils.getParentFeedReader();
+			parentFeedReader.pullAndProcessAllFeeds();
+		} catch (SyncValidationException e) {
+			LOGGER.error("Error during reading feeds: ", e);
+		} catch (Exception e) {
+			LOGGER.error("Error during reading feeds: ", e);
+		}
+		return "module/admin";
 	}
 }
--- omod/src/main/webapp/sync2.jsp
+++ omod/src/main/webapp/sync2.jsp
@@ -1,9 +1,36 @@
 <%@ include file="/WEB-INF/template/include.jsp"%>
-
 <%@ include file="/WEB-INF/template/header.jsp"%>
-
 <%@ include file="template/localHeader.jsp" %>
+<openmrs:htmlInclude file="/moduleResources/sync2/styles/sync2.css"/>
+<openmrs:require anyPrivilege="Sync2 Audit Privilege" otherwise="/login.htm" redirect="/module/sync2/auditList.page"/>
 
-<p>Hello ${user.systemId}!</p>
+<div id="apps">
+    <a class="button app big"
+       href=""
+       id="sync.configuration">
+        <i class="icon-calendar"></i>
+        <spring:message code="sync2.configuration.label"/>
+    </a>
+    <a class="button app big"
+        href="${pageContext.request.contextPath}/module/sync2/manualPull.form"
+        title = ""
+        id="sync.pull">
+        <i class="icon-random"></i>
+        <spring:message code="sync2.sync.manual.pull.label"/>
+    </a>
+    <a class="button app big"
+        href="${pageContext.request.contextPath}/module/sync2/manualPush.form"
+        title = ""
+        id="sync.push">
+        <i class="icon-random"></i>
+        <spring:message code="sync2.sync.manual.push.label"/>
+    </a>
+    <a class="button app big"
+       href=""
+       id="outgoing-message-exceptions.ccd">
+        <i class="icon-calendar"></i>
+        <spring:message code="sync2.audit.list.label"/>
+    </a>
+</div>
 
 <%@ include file="/WEB-INF/template/footer.jsp"%>
