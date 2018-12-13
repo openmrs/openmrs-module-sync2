f94481ab660f954f143637c7bb10832d4066432e
rstencel <44087721+rstencel@users.noreply.github.com>
2018-12-13 12:05:32 +0100

    SYNCT-260 Moved action buttons to Legacy UI (#126)
    * Added 4 action buttons, added controllers for manual push and pull
    
    * Added links to Audit Log and Configuration; added alerts for push and pull operations results (success, failure)
    
    * Code cleaning, enabling custom SyncValidation errors alert messages
    
    * Deleted mocks
    
    * Added jsp page and a controller for it
    
    * Revert "Added jsp page and a controller for it"
    
    This reverts commit 08c77eb5e9444279dc70782f91b41724b5028bb4.

--- omod/src/main/java/org/openmrs/module/sync2/web/controller/Sync2ModuleController.java
+++ omod/src/main/java/org/openmrs/module/sync2/web/controller/Sync2ModuleController.java
@@ -10,20 +10,86 @@
 package org.openmrs.module.sync2.web.controller;
 
 import org.openmrs.api.context.Context;
+import org.openmrs.module.sync2.SyncConstants;
+import org.openmrs.module.sync2.api.exceptions.SyncValidationException;
+import org.openmrs.module.sync2.api.utils.ContextUtils;
+import org.openmrs.module.sync2.api.validator.Errors;
+import org.openmrs.module.sync2.client.reader.LocalFeedReader;
+import org.openmrs.module.sync2.client.reader.ParentFeedReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class Sync2ModuleController {
 
 	protected static final Logger LOGGER = LoggerFactory.getLogger(Sync2ModuleController.class);
+	private static final String USER_MODEL = "user";
+	private static final String ALERT_MESSAGE_MODEL = "alertMessage";
+	private static final String SUCCESS_MESSAGE = "success";
+	private static final String PUSH_SUCCESS_MESSAGE = "sync2.sync.push.success";
+	private static final String PUSH_FAILURE_MESSAGE = "sync2.sync.push.failure";
+	private static final String PULL_SUCCESS_MESSAGE = "sync2.sync.pull.success";
+	private static final String PULL_FAILURE_MESSAGE = "sync2.sync.pull.failure";
 
-	@RequestMapping(value = "/module/sync2/sync2", method = RequestMethod.GET)
+	@RequestMapping(value = "/module/sync2/sync2")
 	public void manage(ModelMap model) {
-		model.addAttribute("user", Context.getAuthenticatedUser());
+		model.addAttribute(USER_MODEL, Context.getAuthenticatedUser());
+	}
+
+	@RequestMapping(value = "/module/sync2/manualPush")
+	public String manualPush(ModelMap model) {
+		try {
+			LOGGER.info("Start Local Feed Reader...");
+			LocalFeedReader localFeedReader = ContextUtils.getLocalFeedReader();
+			localFeedReader.readAndPushAllFeeds();
+			model.put(SUCCESS_MESSAGE, true);
+			model.put(ALERT_MESSAGE_MODEL, PUSH_SUCCESS_MESSAGE);
+		} catch (SyncValidationException e) {
+			LOGGER.error("Error during reading feeds: ", e);
+			model.put(SUCCESS_MESSAGE, false);
+			model.put(ALERT_MESSAGE_MODEL, getFirstErrorCode(e.getErrors()));
+		} catch (Exception e) {
+			LOGGER.error("Error during pushing objects: ", e);
+			model.put(SUCCESS_MESSAGE, false);
+			model.put(ALERT_MESSAGE_MODEL, PUSH_FAILURE_MESSAGE);
+		}
+		return "/module/sync2/sync2";
+	}
+
+	@RequestMapping(value = "/module/sync2/manualPull")
+	public String manualPull(ModelMap model) {
+		try {
+			LOGGER.info("Start Parent Feed Reader...");
+			ParentFeedReader parentFeedReader = ContextUtils.getParentFeedReader();
+			parentFeedReader.pullAndProcessAllFeeds();
+			model.put(SUCCESS_MESSAGE, true);
+			model.put(ALERT_MESSAGE_MODEL, PULL_SUCCESS_MESSAGE);
+		} catch (SyncValidationException e) {
+			LOGGER.error("Error during reading feeds: ", e);
+			model.put(SUCCESS_MESSAGE, false);
+			model.put(ALERT_MESSAGE_MODEL, getFirstErrorCode(e.getErrors()));
+		} catch (Exception e) {
+			LOGGER.error("Error during reading feeds: ", e);
+			model.put(SUCCESS_MESSAGE, false);
+			model.put(ALERT_MESSAGE_MODEL, PULL_FAILURE_MESSAGE);
+		}
+		return "/module/sync2/sync2";
+	}
+
+	@RequestMapping(value = "/module/sync2/auditList")
+	public String auditList() {
+		return "/module/sync2/sync2AuditList";
+	}
+
+	@RequestMapping(value = "/module/sync2/configuration")
+	public String configuration() {
+		return "/module/sync2/sync2Configuration";
+	}
+
+	private String getFirstErrorCode(Errors errors) {
+		return errors.getErrorsCodes().get(SyncConstants.ZERO);
 	}
 }
--- omod/src/main/webapp/sync2.jsp
+++ omod/src/main/webapp/sync2.jsp
@@ -1,9 +1,38 @@
 <%@ include file="/WEB-INF/template/include.jsp"%>
-
 <%@ include file="/WEB-INF/template/header.jsp"%>
-
 <%@ include file="template/localHeader.jsp" %>
+<openmrs:htmlInclude file="/moduleResources/sync2/styles/sync2.css"/>
+<openmrs:require anyPrivilege="Sync2 Audit Privilege" otherwise="/login.htm" redirect="/module/sync2/auditList.page"/>
 
-<p>Hello ${user.systemId}!</p>
+Welcome back <b>${user}</b>!
+<br/><br/>
+<div id="message">
+    <c:if test="${not empty alertMessage}">
+        <c:if test="${success == true}">
+            <span class="success-msg"><spring:message code="${alertMessage}" /></span>
+        </c:if>
+        <c:if test="${success == false}">
+            <span class="failure-msg"><spring:message code="${alertMessage}" /></span>
+        </c:if>
+    </c:if>
+</div>
+<div id="apps">
+    <a class="button"
+        href="${pageContext.request.contextPath}/module/sync2/configuration.form">
+        <spring:message code="sync2.configuration.label"/>
+    </a>
+    <a class="button"
+        href="${pageContext.request.contextPath}/module/sync2/manualPull.form">
+        <spring:message code="sync2.sync.manual.pull.label"/>
+    </a>
+    <a class="button"
+        href="${pageContext.request.contextPath}/module/sync2/manualPush.form">
+        <spring:message code="sync2.sync.manual.push.label"/>
+    </a>
+    <a class="button"
+        href="${pageContext.request.contextPath}/module/sync2/auditList.form">
+        <spring:message code="sync2.audit.list.label"/>
+    </a>
+</div>
 
 <%@ include file="/WEB-INF/template/footer.jsp"%>
