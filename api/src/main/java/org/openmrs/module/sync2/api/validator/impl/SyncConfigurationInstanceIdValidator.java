372fe7946ade3a4df8b515af52bbcd76ead8a568
areklalo <alalo@soldevelo.com>
2019-01-07 10:37:43 +0100

    SYNCT-262: Improved the configuration validation process

--- api/src/main/java/org/openmrs/module/sync2/api/validator/impl/SyncConfigurationInstanceIdValidator.java
+++ api/src/main/java/org/openmrs/module/sync2/api/validator/impl/SyncConfigurationInstanceIdValidator.java
@@ -2,6 +2,7 @@ package org.openmrs.module.sync2.api.validator.impl;
 
 import org.apache.commons.lang.StringUtils;
 import org.openmrs.module.sync2.api.model.configuration.SyncConfiguration;
+import org.openmrs.module.sync2.api.utils.SyncUtils;
 import org.openmrs.module.sync2.api.validator.Errors;
 import org.openmrs.module.sync2.api.validator.SyncConfigurationValidator;
 import org.springframework.stereotype.Component;
@@ -10,11 +11,20 @@ import org.springframework.stereotype.Component;
 public class SyncConfigurationInstanceIdValidator implements SyncConfigurationValidator {
 
     private static final String ERROR_CODE = "sync2.error.instanceIdIsNotSet";
+    private static final String PARENT_URI_ERROR = "sync2.configuration.parentUrl.empty";
 
     @Override
     public void validate(SyncConfiguration syncConfiguration, Errors errors) {
         if (StringUtils.isBlank(syncConfiguration.getGeneral().getLocalInstanceId())) {
             errors.addErrorCode(ERROR_CODE);
         }
+        if (parentInstanceUriIsEmpty()) {
+            errors.addErrorCode(PARENT_URI_ERROR);
+        }
     }
+
+    private boolean parentInstanceUriIsEmpty() {
+        return StringUtils.isBlank(SyncUtils.getParentBaseUrl(null));
+    }
+
 }
