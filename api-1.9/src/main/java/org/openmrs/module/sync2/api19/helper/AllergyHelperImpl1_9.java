2aa25a08d760252673d69336d13b51fd4c2687de
dkayiwa <kayiwadaniel@gmail.com>
2019-07-31 21:48:34 +0300

    Adding support for platform 1.12.x

--- api-1.9/src/main/java/org/openmrs/module/sync2/api19/helper/AllergyHelperImpl1_9.java
+++ api-1.9/src/main/java/org/openmrs/module/sync2/api19/helper/AllergyHelperImpl1_9.java
@@ -6,7 +6,7 @@ import org.openmrs.module.sync2.api.helper.AllergyHelper;
 import org.springframework.stereotype.Component;
 
 @Component(value = "sync2.AllergyHelper1_9")
-@OpenmrsProfile(openmrsPlatformVersion = "1.9.* - 1.11.*")
+@OpenmrsProfile(openmrsPlatformVersion = "1.9.* - 1.12.*")
 public class AllergyHelperImpl1_9 implements AllergyHelper {
 
 	@Override
